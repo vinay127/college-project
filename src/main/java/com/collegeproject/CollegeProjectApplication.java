ebbb769bee53fc5d6dbfd52420367938ea022e28
Vinay <you@example.com>
2018-08-02 18:14:00 +0530

    Initial Server Code

---
+++ src/main/java/com/collegeproject/CollegeProjectApplication.java
@@ -0,0 +1,12 @@
+package com.collegeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeProjectApplication.class, args);
+	}
+}
